c3daa91cd86c7b3f4522d9e379a33a2c87b666f0
Allene Norton <norton.allene@gmail.com>
2020-07-02 03:44:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -187,7 +187,7 @@ def player_stats(player)
   game_hash.each do |home_away, categories|
     categories[:players].each do |player_array|
       if player_array[:player_name] == player
-        da_stats = categories[:players].each_with_object({}) {|key, value| key => value}
+        da_stats = player_array.collect {|player_array| player_array[0...8]}
       end 
     end 
     da_stats
