3e013f9804b9f92e5fed753826e8ca18b1dfcc5f
Allene Norton <norton.allene@gmail.com>
2020-07-02 03:36:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -190,7 +190,7 @@ def player_stats(player)
         da_stats = player_array.collect {|player_array| player_array[0...8]}
       end 
     end 
-    da_stats.[0][2].to_h
+    da_stats[0][2].to_h
   end
   #binding.pry 
 end 
